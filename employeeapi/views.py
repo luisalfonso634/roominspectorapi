28b6ed4238e56913c10b18ead5ac1e819ca92f73
Luis Fernandez (Kayros) <luisalfonso634@gmail.com>
2022-03-11 19:54:09 -0300

    6to commit login&logout

--- employeeapi/views.py
+++ employeeapi/views.py
@@ -4,6 +4,11 @@ from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import EmployeeSerializer, RegisterSerializer
 
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from django.contrib.auth import login
+
 
 # Register API
 class RegisterAPI(generics.GenericAPIView):
@@ -17,4 +22,15 @@ class RegisterAPI(generics.GenericAPIView):
         "user": EmployeeSerializer(user, context=self.get_serializer_context()).data,
         "token": AuthToken.objects.create(user)[1]
         })
-        
+
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)   
--- roominspector/urls.py
+++ roominspector/urls.py
@@ -16,11 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .router import router
-from employeeapi.views import RegisterAPI
+from employeeapi.views import RegisterAPI, LoginAPI
+
+from knox import views as knox_views
+from django.urls import path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
