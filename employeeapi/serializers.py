4ad668c00219225b7bd351ce8b990508268903c2
Luis Fernandez (Kayros) <luisalfonso634@gmail.com>
2022-03-11 18:20:51 -0300

    first commit for serializer

--- employeeapi/serializers.py
+++ employeeapi/serializers.py
@@ -1,9 +1,40 @@
 #api ==> mobile app / web api / json
 from rest_framework import serializers
 from .models import Employee
+from django.contrib.auth.models import User
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model=Employee
         fields = '__all__'
-    
+
+
+# Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('id', 'user_email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['email'], validated_data['password'])
+
+        return user
+
+# User Serializer    
+#class UserSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = User
+#        fields = ('id','username','email')
+
+# Register Serializer
+# class RegisterSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id','username','email','password')
+#         extra_kwargs = {'password': {'write_only': True}} 
+
+#     def create(self, validated_data):
+#         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+
+#         return user      
