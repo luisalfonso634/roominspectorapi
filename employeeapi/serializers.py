d623957c7cca414194f7ccdcc32bf510d3659856
Luis Fernandez (Kayros) <luisalfonso634@gmail.com>
2022-03-11 19:20:41 -0300

    5to commit POST login

--- employeeapi/serializers.py
+++ employeeapi/serializers.py
@@ -17,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(validated_data['user_email'], validated_data['password'])
 
         return user
 
--- roominspector/urls.py
+++ roominspector/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from .router import router
 from employeeapi.views import RegisterAPI
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
