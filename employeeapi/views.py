ff38239ef50c260f4d8d62d688071e39861f85bf
Luis Fernandez (Kayros) <luisalfonso634@gmail.com>
2022-03-11 18:43:17 -0300

    3er commit Views for Loggin

--- employeeapi/views.py
+++ employeeapi/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+#Views Login
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import EmployeeSerializer, RegisterSerializer
 
-# Create your views here.
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": EmployeeSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
+        
